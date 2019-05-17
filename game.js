44ef880419a2b055850bf4e3be64f21a477abca6
Subodh Dhakal <subodh.dhakal@mavs.uta.edu>
2019-05-17 15:44:23 -0500

    startover() function added

--- game.js
+++ game.js
@@ -54,6 +54,22 @@ function checkAnswer(currentLevel) {
             },1000);
         }
     }
-    else
+    else {
         console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 500);
+
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver(currentLevel, gamePattern, userClickedPattern);
+    }
+}
+
+function startOver(currentLevel, gamePattern, userClickedPattern) {
+    i = 0;
+    gamePattern.length = 0;
+    userClickedPattern.length = 0;
 }

8f396ee5a79a6501b3657fa4a136fed5db80995c
Subodh Dhakal <subodh.dhakal@mavs.uta.edu>
2019-05-17 12:39:23 -0500

    onclick listner added

--- game.js
+++ game.js
@@ -1,1 +1,24 @@
-alert("Linked");
+var gamePattern = [];
+var userClickedPattern = [];
+
+    var buttonColours = ['red', 'blue', 'green', 'yellow'];
+    var randomNumber = Math.floor(Math.random()*4);
+    console.log(randomNumber);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+
+    $(".btn").click(function(event) {
+       var userChosenColour = this.id;
+        userClickedPattern.push(userChosenColour);
+        console.log(userClickedPattern);
+        playSound(this.id);
+    });
+
+    function playSound(name) {
+        var audio = new Audio("/sounds/"+name+".mp3");
+        audio.play();
+    }
+    
